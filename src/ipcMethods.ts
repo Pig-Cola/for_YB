9e0b4765ce7354c78c0395fe557362b20f1e986e
Dae-seong Hwang <co.la@kakao.com>
2024-05-20 01:24:34 +0900

    fix(save): encoding change
    저장되는 encoding UTF-16LE로 고정

--- src/ipcMethods.ts
+++ src/ipcMethods.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs'
 
 ipcMain.handle( 'saveJson', ( e, obj: jsonFileType, filePath: File['path'] ) => {
   const json = JSON.stringify( obj, undefined, 4 )
-  fs.writeFileSync( filePath, json, { flush: true } )
+  fs.writeFileSync( filePath, json, { flush: true, encoding: 'utf-16le' } )
   return json
 } )
 
