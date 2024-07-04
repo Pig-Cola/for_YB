0c7389fc6e480d7bf3ba20687cc02872b31431f7
Hwang_DaeSeong <-g>
2024-07-04 20:37:46 +0900

    refactor(bundler): change
    webpack -> vite

--- forge.config.ts
+++ forge.config.ts
@@ -5,10 +5,7 @@ import { MakerSquirrel } from '@electron-forge/maker-squirrel'
 import { MakerZIP } from '@electron-forge/maker-zip'
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives'
 import { FusesPlugin } from '@electron-forge/plugin-fuses'
-import { WebpackPlugin } from '@electron-forge/plugin-webpack'
-
-import { mainConfig } from './webpack.main.config'
-import { rendererConfig } from './webpack.renderer.config'
+import { VitePlugin } from '@electron-forge/plugin-vite'
 
 import type { ForgeConfig } from '@electron-forge/shared-types'
 
@@ -26,21 +23,26 @@ const config: ForgeConfig = {
   ],
   plugins: [
     new AutoUnpackNativesPlugin( {} ),
-    new WebpackPlugin( {
-      mainConfig,
-      renderer: {
-        config: rendererConfig,
-        entryPoints: [
-          {
-            html: './src/index.html',
-            js: './src/renderer.ts',
-            name: 'main_window',
-            preload: {
-              js: './src/preload.ts',
-            },
-          },
-        ],
-      },
+    new VitePlugin( {
+      // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
+      // If you are familiar with Vite configuration, it will look really familiar.
+      build: [
+        {
+          // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
+          entry: 'src/index.ts',
+          config: 'vite.main.config.ts',
+        },
+        {
+          entry: 'src/preload.ts',
+          config: 'vite.preload.config.ts',
+        },
+      ],
+      renderer: [
+        {
+          name: 'main_window',
+          config: 'vite.renderer.config.ts',
+        },
+      ],
     } ),
     // Fuses are used to enable/disable various Electron functionality
     // at package time, before code signing the application
