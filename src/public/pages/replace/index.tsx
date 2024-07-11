75aeba0064ac971dfa22816ad7ef9d46d00c15c8
Hwang_DaeSeong <-g>
2024-07-11 16:21:03 +0900

    refactor(component): `pages/replace` 내용 일부 `components/menu` 로 분리

--- src/public/pages/replace/index.tsx
+++ src/public/pages/replace/index.tsx
@@ -1,20 +1,14 @@
 import { useEffect, useRef, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 
-import { Button } from '@nextui-org/button'
-import { Tooltip } from '@nextui-org/tooltip'
 import { useDisclosure } from '@nextui-org/use-disclosure'
-import enc from 'encoding-japanese'
 import { Reorder } from 'framer-motion'
-import { default as _get } from 'lodash/get'
 import partition from 'lodash/partition'
 
 import { useIpcRenderer } from '@/hooks/useIpcRenderer'
 import { LeaderBoardItemSlot } from '@/components/ItemSlot'
 import { LeaderBoardSetting } from '@/components/leaderBoardSetting'
-import { MyIcon } from '@/components/my-icon'
+import Menu from '@/components/menu'
 import { useFileStore } from '@/zustand/fileStore'
-import { useSettingForLeaderBoard } from '@/zustand/settingForLeaderBoard'
 import { getCarModelString } from '@/utill/getCarModel'
 
 import styles from './index.module.scss'
@@ -23,21 +17,25 @@ import { classOption } from '@/utill/class-helper'
 const { classname } = classOption( styles )
 
 export default function Replace() {
-  const navi = useNavigate()
   const { ipcRenderer } = useIpcRenderer()
   const settingOptions = useDisclosure()
   const mainRef = useRef<HTMLDivElement>( null )
 
   const [fileReload, doFileReload] = useState( 0 )
-  const { file, setFile } = useFileStore( ( s ) => s ) // 선택된 파일 객체
+  const { file } = useFileStore( ( s ) => s ) // 선택된 파일 객체
   const [text, setText] = useState( '' ) // 파일 raw text
   const [obj, setObj] = useState<jsonFileTypeEx>() // json 객체
-
-  const { userProperties } = useSettingForLeaderBoard()
 
   // 순위 결과 객체
   const [leaderBoard, setLeaderBoard] = useState<jsonFileTypeEx['sessionResult']['leaderBoardLines']>()
   const [invalidLeaderBoard, setInvalidLeaderBoard] = useState<jsonFileTypeEx['sessionResult']['leaderBoardLines']>()
+
+  const [penalty, setPenalty] = useState( {} as Record<string, `${number}`> )
+  useEffect( () => {
+    void fileReload
+
+    setPenalty( {} )
+  }, [fileReload] )
 
   // file read
   useEffect( () => {
@@ -82,7 +80,7 @@ export default function Replace() {
   return (
     <main className={classname( ['main'] )} ref={mainRef}>
       {/* menu-start */}
-      <div className={classname( ['menu'] )}>
+      {/* <div className={classname( ['menu'] )}>
         <Tooltip content="설정">
           <Button size="sm" onPress={settingOptions.onOpen}>
             <MyIcon>cog</MyIcon>
@@ -106,7 +104,14 @@ export default function Replace() {
                   new Blob(
                     [
                       enc.codeToString(
-                        enc.convert( new Uint16Array( enc.stringToCode( JSON.stringify( newS, undefined, 2 ) ) ), 'UTF16LE' ),
+                        enc.convert(
+                          new Uint16Array(
+                            enc.stringToCode(
+                              JSON.stringify( newS, ( key, value ) => ( key !== 'carModelString' ? value : undefined ), 2 ),
+                            ),
+                          ),
+                          'UTF16LE',
+                        ),
                       ),
                     ],
                     {
@@ -190,6 +195,33 @@ export default function Replace() {
           </Button>
         </Tooltip>
 
+        <Tooltip color="primary" placement="bottom" content="일부 기능만 작동">
+          <Button
+            color="primary"
+            size="sm"
+            onPress={() => {
+              if ( !confirm( '기존 정렬을 무시하고 재정렬 됩니다.' ) ) return
+
+              setLeaderBoard( ( s ) => {
+                const temp = s
+                  .toSorted(
+                    ( a, b ) =>
+                      a.timing.totalTime +
+                      ( +penalty[a.currentDriver.playerId] || 0 ) -
+                      ( b.timing.totalTime + ( +penalty[b.currentDriver.playerId] || 0 ) ),
+                  )
+                  .toSorted( ( a, b ) => b.timing.lapCount - a.timing.lapCount )
+
+                console.log( s )
+                console.log( temp )
+                return temp
+              } )
+            }}
+          >
+            사용자 정렬
+          </Button>
+        </Tooltip>
+
         <Button
           color="secondary"
           size="sm"
@@ -200,7 +232,18 @@ export default function Replace() {
         >
           홈으로
         </Button>
-      </div>
+      </div> */}
+      <Menu
+        {...{
+          doFileReload,
+          invalidLeaderBoard,
+          leaderBoard,
+          penalty,
+          setLeaderBoard,
+          setObj,
+          settingOptions,
+        }}
+      />
       {/* menu-end */}
 
       <div className={classname( ['leaderBoardLines-wrapper'] )}>
@@ -217,8 +260,9 @@ export default function Replace() {
               value={v}
               index={i}
               key={`${v.car.carId}-${fileReload}`}
-              max={leaderBoard.length}
-              reorder={setLeaderBoard}
+              // max={leaderBoard.length}
+              // reorder={setLeaderBoard}
+              setPenalty={setPenalty}
             />
           ) )}
         </Reorder.Group>
@@ -245,6 +289,11 @@ export default function Replace() {
           <span className={classname( ['title'] )}>트랙 : </span>
           {obj?.trackName}
         </p>
+
+        <div className={classname( ['noti'] )}>
+          <span style={{ backgroundColor: '#006fee', color: '#fff', padding: '0.1rem' }}>사용자 정렬</span>기능은 현재
+          같은 랩 수에서 'TotalTime + 페널티'의 오름차순 정렬만 지원합니다.
+        </div>
       </div>
 
       <LeaderBoardSetting
